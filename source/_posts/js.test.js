9993f8b46fb1d5e662f144f6506e2313fde04b2e
heybigfish <327860593@qq.com>
2021-08-10 19:38:59 +0800

    init

--- source/_posts/js.test.js
+++ source/_posts/js.test.js
@@ -1,11 +1,3 @@
-/*
- * @Author: your name
- * @Date: 2021-08-02 18:03:30
- * @LastEditTime: 2021-08-10 18:54:55
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \heybigfish.github.io\source\_posts\js.test.js
- */
 var a = "123.111";
 console.log(parseInt(a)); // 123
 var b = true;
